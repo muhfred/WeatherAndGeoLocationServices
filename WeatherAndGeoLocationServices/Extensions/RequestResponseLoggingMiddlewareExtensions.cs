fd466101898ea7085d64640c30f79110d507e15c
muhammad fred <muhfred@outlook.com>
2019-11-23 22:31:09 +0200

    Add project files.

---
+++ WeatherAndGeoLocationServices/Extensions/RequestResponseLoggingMiddlewareExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using WeatherAndGeoLocationServices.Middleware;
+
+namespace WeatherAndGeoLocationServices.Extensions
+{
+    public static class RequestResponseLoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestResponseLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestResponseLoggingMiddleware>();
+        }
+    }
+}
