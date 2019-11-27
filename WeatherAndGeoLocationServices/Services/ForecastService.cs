89e62891e21632536a2eda230c5df918f106e192
muhammad fred <muhfred@outlook.com>
2019-11-27 08:47:55 +0200

    remove and sort using

--- WeatherAndGeoLocationServices/Services/ForecastService.cs
+++ WeatherAndGeoLocationServices/Services/ForecastService.cs
@@ -3,7 +3,6 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using WeatherAndGeoLocationServices.Config;
-using WeatherAndGeoLocationServices.Models.Weather;
 
 namespace WeatherAndGeoLocationServices.Services
 {
--- WeatherAndGeoLocationServices/Services/GeoLocationService.cs
+++ WeatherAndGeoLocationServices/Services/GeoLocationService.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using WeatherAndGeoLocationServices.Config;
 
 namespace WeatherAndGeoLocationServices.Services
